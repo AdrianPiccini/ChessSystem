daeefc5e8f99b9d043c2f8930890b24211599e48
Adrian Piccini <adrianmormaii@gmail.com>
2021-09-09 22:01:41 -0300

    Special Move: Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,11 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				if(chessMatch.getPromoted() != null) {
+					System.out.print("Enter a piece of Promotion: (B/H/Q/R): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
 			}
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
